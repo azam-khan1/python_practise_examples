49e7556f1749c399f53976c8a2b1f5f1a5c05691
Azam Farooq Khan <azamfarooqkhan@gmail.com>
2026-04-01 09:23:37 +0500

    adding data

---
+++ example_datatypes/list.py
@@ -0,0 +1,6 @@
+names = ["asad","azam","hashim","ahsan","saifullah"]
+print(f"The Names are: ",names)
+names.append("sajid")
+print(f"The Names are: ",names)
+names.sort()
+print(f"Sorted Names are: ",names )
